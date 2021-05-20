cbd2662799be2100e17f423c6d36d73719a338e2
Fuad <fuad-udabol@github.com>
2021-05-20 16:41:29 -0400

    Changes for database

--- database/notaria.sql
+++ database/notaria.sql
@@ -16,6 +16,57 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `procedure_types`
+--
+
+DROP TABLE IF EXISTS `procedure_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `procedure_types` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `procedure_types`
+--
+
+LOCK TABLES `procedure_types` WRITE;
+/*!40000 ALTER TABLE `procedure_types` DISABLE KEYS */;
+INSERT INTO `procedure_types` VALUES (1,'Escritura Pública'),(2,'Poder'),(3,'Reconocimiento de firmas'),(4,'Declaraciones Voluntarias'),(5,'Carta Notarial'),(6,'Acta Notarial'),(7,'Copia Legalizada'),(8,'Certificado de Vivencia'),(9,'Protesto'),(10,'Autorización de Viaje');
+/*!40000 ALTER TABLE `procedure_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `procedures`
+--
+
+DROP TABLE IF EXISTS `procedures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `procedures` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `procedure_number` int DEFAULT NULL,
+  `procedure_date` varchar(50) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `procedure_type_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `procedures`
+--
+
+LOCK TABLES `procedures` WRITE;
+/*!40000 ALTER TABLE `procedures` DISABLE KEYS */;
+INSERT INTO `procedures` VALUES (2,4,'1621481451.867',1,10),(4,2,'1621482923.41',1,8),(11,1,'1621484101.764',1,8),(12,12,'1621484130.41',3,9);
+/*!40000 ALTER TABLE `procedures` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -26,8 +77,11 @@ CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_name` varchar(100) NOT NULL,
   `user_email` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `user_ci` varchar(45) NOT NULL,
+  `user_last_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_ci_UNIQUE` (`user_ci`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +90,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'asdf','asd@asdf.coas');
+INSERT INTO `users` VALUES (1,'Fuad','fuadsalo@gmail.asdf','23422234','Salomon'),(3,'Jose Miguel','asdf@asda.com','234234','Sanchez');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-13 22:21:13
+-- Dump completed on 2021-05-20 16:40:54
